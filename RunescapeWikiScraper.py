363399b320e91e83d880f3c9851802f825d52870
Dieter <dieter.decaestecker@test.com>
2020-01-17 14:04:41 +0100

    Better parsing of entity name for the ?armour command + cleaned up the existing implementation

--- RunescapeWikiScraper.py
+++ RunescapeWikiScraper.py
@@ -1,16 +1,34 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_armour(nameOfEntity):
-    page = requests.get(
-        "https://runescape.wiki/api.php?action=query&prop=revisions&rvprop=content&titles=" + nameOfEntity + "&format=json")
-    soup = BeautifulSoup(page.content, 'html.parser')
+    request_response = requests.get(build_url(nameOfEntity))
+    parsed_response = str(BeautifulSoup(request_response.content, 'html.parser'))
 
-    html = str(soup)
+    # the armour attribute looks like this:
+    # armour = 1924\n|
+    # armour1 = 1486|armour2 = 1486| ...
+    start_of_attribute = parsed_response.find("armour")
+    end_of_attribute = start_of_attribute + parsed_response[start_of_attribute:].find('|')
+    start_of_armour_value = start_of_attribute + parsed_response[start_of_attribute:end_of_attribute].rfind(" ")
 
-    startOfArmourBullshit = html.find("armour")
-    firstPipeThing = startOfArmourBullshit + html[startOfArmourBullshit:].find('|')
-    lastSpace = startOfArmourBullshit + html[startOfArmourBullshit:firstPipeThing].rfind(" ")
+    return parsed_response[start_of_armour_value:end_of_attribute].replace("\\n", "")
 
-    return html[lastSpace:firstPipeThing].replace("\\n", "")
+
+def build_url(nameOfEntity):
+    runescape_wiki_entity_name = to_runescape_wiki_entity_name(nameOfEntity)
+    return "https://runescape.wiki/api.php?action=query&prop=revisions&rvprop=content&titles=" + runescape_wiki_entity_name + "&format=json"
+
+
+def to_runescape_wiki_entity_name(rawName):
+    words_in_entity_name = re.sub("[ ]+", " ", rawName).strip().split(" ")
+
+    words_all_lower = [word.lower() for index, word in enumerate(words_in_entity_name)]
+
+    if len(words_all_lower) > 0:
+        words_all_lower[0].capitalize()
+
+    return "_".join(words_all_lower)
